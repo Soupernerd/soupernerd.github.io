4791636f82ad7432117e29c8aba8b30019772f88
Soupernerd <17029787+Soupernerd@users.noreply.github.com>
2022-12-15 00:53:19 -0600

    Update sponsors.js

--- js/sponsors.js
+++ js/sponsors.js
@@ -1,31 +1,3 @@
-function getImageInfo() {
-  return fetch("json/sponsors.json")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      // Instead of accessing the nonexistent "keySponsors" property,
-      // access the array of objects directly
-      var entries = Object.entries(data);
-
-      var imageInfo = entries.map(function(entry) {
-        var key = entry[0];
-        var object = entry[1];
-
-        return {
-          src: object.src,
-          href: object.href
-        };
-      });
-
-      localStorage.setItem("sponsors", JSON.stringify(imageInfo));
-      return imageInfo;
-    })
-    .catch(function(error) {
-      console.error(error);
-    });
-}
-
 function displayImages() {
   // Log a message to the JavaScript console
   console.log("Displaying images on the page");
@@ -58,7 +30,6 @@ function displayImages() {
   });
 }
 
-
 function cycleImages() {
   var imageInfo = JSON.parse(localStorage.getItem("sponsors"));
 
@@ -80,8 +51,6 @@ function cycleImages() {
     img.parentNode.href = imageInfo[nextIndex].href;
   }
 }
-
-
 
 // When the page loads, get the image names and URLs and store them in local storage
 window.addEventListener("load", function() {
